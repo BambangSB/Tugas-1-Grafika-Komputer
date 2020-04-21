874f6790bd74ff87edc1478f2a75a5ea366894c4
BambangSB <61703343+BambangSB@users.noreply.github.com>
2020-04-21 16:14:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "maroon";
-ctx.fillRect(50,100,100,150);
+canvas.width = canvas.scrollWidth;
+canvas.height = canvas.scrollHeight;
 
+
+// Create gradient
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"black");
+grd.addColorStop(1,"yellow");
+
+ctx.fillStyle = grd;
+ctx.fillRect(140,190,100,150);
+
+
+
+// warna biasa
+// ctx.fillStyle = "maroon";
+// ctx.fillRect(140,190,100,150);
+
+
+function tutup(){
+ctx.translate(90, 90);
 ctx.beginPath();
 ctx.moveTo(50,100);
 ctx.lineTo(50,80);
@@ -13,4 +31,46 @@ ctx.lineTo(110,80);
 ctx.lineTo(150,80);
 ctx.lineTo(150,100);
 ctx.closePath();
+ctx.fillStyle = "black";
+ctx.fill();
 ctx.stroke();
+}
+//tutup();
+
+function buka1(){
+ctx.translate(203, 11);
+ctx.rotate(Math.PI/4);
+ctx.moveTo(50,100);
+ctx.lineTo(50,80);
+ctx.lineTo(90,80);
+ctx.lineTo(90,60);
+ctx.lineTo(110,60);
+ctx.lineTo(110,80);
+ctx.lineTo(150,80);
+ctx.lineTo(150,100);
+ctx.closePath();
+ctx.fillStyle = "black";
+ctx.fill();
+ctx.stroke();
+}
+buka1();
+
+function buka2(){
+ctx.translate(320, 40);
+ctx.rotate(Math.PI/2);
+ctx.moveTo(50,100);
+ctx.lineTo(50,80);
+ctx.lineTo(90,80);
+ctx.lineTo(90,60);
+ctx.lineTo(110,60);
+ctx.lineTo(110,80);
+ctx.lineTo(150,80);
+ctx.lineTo(150,100);
+ctx.closePath();
+ctx.fillStyle = "black";
+ctx.fill();
+ctx.stroke();
+
+
+}
+//buka2();
